0f027b2fff84d43149d33103a9ff1dcf035e46c7
lqy407763361 <407763361@qq.com>
2025-07-10 13:40:29 +0800

    0710

--- src/components/index/common/CommonHeader.vue
+++ src/components/index/common/CommonHeader.vue
@@ -44,7 +44,6 @@ const changeLoginPopup = (status, loginType) => {
                         <li><a href="/article">文章</a></li>
                         <li><a href="/about">关于</a></li>
                   </ul>
-<<<<<<< HEAD
                   <ul class="login-nav">
                         <li>
                               <a @click="changeLoginPopup(true, 'login')">
@@ -60,33 +59,6 @@ const changeLoginPopup = (status, loginType) => {
       </header>
       <div class="mask" :class="{'fade':maskStatus}" @click="closeAll(false)"></div>
       <CommonLogin ref="commonLoginRef" />
-=======
-            </nav>
-            <header>
-                  <div class="container">
-                        <h1><a href="/">L-BLOG</a></h1>
-                        <ul class="pc-nav">
-                              <li class="nav-active"><a href="/">首页</a></li>
-                              <li><a href="/article">文章</a></li>
-                              <li><a href="/about">关于</a></li>
-                        </ul>
-                        <ul class="login-nav">
-                              <li>
-                                    <a @click="changeLoginPopup(true, 'login')">
-                                    <User style="width:2rem;height:2rem;float:left;"/>
-                                    &nbsp;&nbsp;登录</a>
-                                    /
-                                    <a @click="changeLoginPopup(true, 'register')">注册</a>
-                              </li>
-                        </ul>
-                        <Operation class="mobile-nav-button" @click="changeMobileNav(true)" />
-                        <div style="clear:both;"></div>
-                  </div>
-            </header>
-            <div class="mask" :class="{'fade':maskStatus}" @click="closeAll(false)"></div>
-            <CommonLogin ref="commonLoginRef" />
-      </div>
->>>>>>> 4c223aaddd307c21f1e98e121aefcbd7c352c90f
 </template>
 
 <style scoped>
