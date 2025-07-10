ab59254b2fc408807072702370785363d3ab81ce
lqy407763361 <407763361@qq.com>
2025-07-10 15:15:46 +0800

    0710

--- src/router/admin.js
+++ src/router/admin.js
@@ -4,6 +4,11 @@ const routes = [
         name: 'AdminIndex',  
         component: import('@/components/admin/index/IndexPage.vue'),
     },
+    { 
+        path: '/admin/test',
+        name: 'AdminTest',  
+        component: import('@/components/admin/index/TestPage.vue'),
+    },
 ]
 
 export default routes
