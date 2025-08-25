1322a2d22d69c29be30db353699780ca25dec22d
lqy407763361 <407763361@qq.com>
2025-08-25 16:02:07 +0800

    0825

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -48,7 +48,7 @@ const changeLoginPopup = (status, loginType) => {
                   <li class="nav-active"><router-link href="/">首页</router-link></li>
                   <li><router-link to="/article">文章</router-link></li>
                   <li><router-link to="/about">关于</router-link></li>
-                  <li><span @click="changeLoginPopup(true, 'login')">登录</span>&nbsp;&nbsp;&nbsp;&nbsp;<span @click="changeLoginPopup(true, 'register')">注册</span></li>
+                  <li><a @click="changeLoginPopup(true, 'login')">登录</a>&nbsp;&nbsp;&nbsp;&nbsp;<a @click="changeLoginPopup(true, 'register')">注册</a></li>
             </ul>
       </nav>
       <header>
@@ -61,16 +61,16 @@ const changeLoginPopup = (status, loginType) => {
                   </ul>
                   <ul class="login-nav">
                         <li>
-                              <span @click="changeLoginPopup(true, 'login')">
+                              <a @click="changeLoginPopup(true, 'login')">
                               <font-awesome-icon icon="fa-solid fa-right-to-bracket" />
-                              &nbsp;&nbsp;登录</span>
+                              &nbsp;&nbsp;登录</a>
                               /
-                              <span @click="changeLoginPopup(true, 'register')">注册</span>
+                              <a @click="changeLoginPopup(true, 'register')">注册</a>
                         </li>
                         <!-- <li>
                               您好！张三
                               /
-                              <span @click="changeLoginOut()">退出</span>
+                              <a @click="changeLoginOut()">退出</a>
                         </li> -->
                   </ul>
                   <font-awesome-icon icon="fa-solid fa-bars" class="mobile-nav-button" @click="changeMobileNav(true)"/>
